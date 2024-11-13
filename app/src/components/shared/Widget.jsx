b75b1524cc608e50270def53ac3e4350b2a7e367
IlariaM <ilaria.mammana15@gmail.com>
2024-11-13 10:31:51 +0100

    merged with ludovica

---
+++ app/src/components/shared/Widget.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Widget = () => {
+  return (
+    <>
+        <div className="m-5 rounded-lg bg-white w-full shadow p-4">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ex, officia quisquam modi inventore suscipit, harum, laborum ad consequatur ratione accusantium impedit sapiente reprehenderit. Beatae, tenetur! Earum quae consectetur saepe.</p>
+        </div>
+    </>
+  )
+}
+
+export default Widget
