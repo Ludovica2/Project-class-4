811ff4378d99b2a5d2aec160b7ca9fbf7135b0f2
alexdant91 <alexdant91@gmail.com>
2024-11-18 17:36:55 +0100

    Merged ludovica

--- app/src/components/shared/CustomBox.jsx
+++ app/src/components/shared/CustomBox.jsx
@@ -1,9 +1,4 @@
 import React, { useState } from 'react'
-<<<<<<< HEAD
-
-const CustomBox = ({ children, divider = false, title = "" }) => {
-    const [isOpenComments, setIsOpenComments] = useState(false);
-=======
 import ContentEditable from "react-contenteditable";
 import { motion } from "framer-motion"
 
@@ -15,43 +10,11 @@ const CustomBox = ({ children, divider = false, profile = "", imgProfile = "", d
     const handleChange = (event) => {
         setField(event.target.value);
     }
->>>>>>> origin/ludovica
 
     const toggleCommentsBox = () => {
         setIsOpenComments((isOpen) => !isOpen);
     }
 
-<<<<<<< HEAD
-    return (
-        <>
-            <div className="m-5 rounded-lg bg-white w-full shadow">
-                <div className="rounded-t-lg">
-                    <h3 className="p-4">{title}</h3>
-                    {divider && <div className="w-full h-[1px] mb-5 bg-slate-100"></div>}
-                </div>
-                <div className="m-4 p-1 rounded-b-lg">
-                    {children}
-                </div>
-                <div className="flex flex-col mt-6 p-4 border border-slate-100 rounded-b-lg">
-                    <div className='flex justify-between'>
-                        <div>
-                            <button className="p-2 mr-2">
-                                <i className="fa-solid fa-camera text-secondaryColor text-xl hover:text-secondaryColor_Hover"></i>
-                            </button>
-                            <button className="p-2 mr-2">
-                                <i className="fa-solid fa-video text-secondaryColor text-xl hover:text-secondaryColor_Hover"></i>
-                            </button>
-                            <button className="p-2 mr-2">
-                                <i className="fa-solid fa-location-dot text-secondaryColor text-xl hover:text-secondaryColor_Hover"></i>
-                            </button>
-                        </div>
-                        <button className="btn" onClick={toggleCommentsBox}>Commenta</button>
-                    </div>
-                    {
-                        isOpenComments && (
-                            <div className='p-4 '>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi repellat aliquid, explicabo ipsa adipisci molestias illo temporibus id, cupiditate tempore magnam suscipit! Aut illo veritatis quo. Id dolor explicabo temporibus.</p>
-=======
     const toggleReplyComments = () => {
         setReplyComments((isOpen) => !isOpen);
     }
@@ -199,7 +162,6 @@ const CustomBox = ({ children, divider = false, profile = "", imgProfile = "", d
                                         </motion.button>
                                     </div>
                                 </div>
->>>>>>> origin/ludovica
                             </div>
                         )
                     }
